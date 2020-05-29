fedb2cdb321e91a0e057a0e3aed7747c91a2bc1b
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2020-05-30 01:53:54 +0700

    [Por] - provide description for each API signature

--- src/main/java/com/pp/classy/controllers/ClassController.java
+++ src/main/java/com/pp/classy/controllers/ClassController.java
@@ -8,32 +8,48 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClassController {
 
-  //teacher
+
+  /**
+   * For Teacher,
+   * Get All classes he/she created based on user id
+   */
   @GetMapping
   public void getAllClasses() {
-
   }
 
-  //teacher
+  /**
+   * For Teacher,
+   * Create a classroom given user id
+   */
   @PostMapping
   public void createClass() {
 
   }
 
-  //teacher
+  /**
+   * For Teacher,
+   * Generate a QR code for students to attend
+   * QR code is basically created by provide a link to attendances URL
+   */
   @GetMapping("/{classId}/qr")
   public void generateQR(@PathVariable Long classId) {
 
   }
 
 
-  //teacher
+  /**
+   * For Teacher,
+   * Generate XML/JSON/CSV list of students that attended given class
+   */
   @GetMapping("/{classId}/attendances")
   public void generateClassAttendance(@PathVariable Long classId) {
 
   }
 
-  //student
+  /**
+   * For Student,
+   * Create an attendance record into database
+   */
   @PostMapping("/{classId}/students/{studentId}")
   public void createAttendance(@PathVariable Long classId, @PathVariable Long studentId) {
 
