7ad4c89a85f1239ff6cc582f10f3745a401715c9
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2020-05-30 01:38:25 +0700

    [Por] create API signature for required actions

--- src/main/java/com/pp/classy/controllers/ClassController.java
+++ src/main/java/com/pp/classy/controllers/ClassController.java
@@ -1,19 +1,41 @@
 package com.pp.classy.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClassController {
 
+  //teacher
   @GetMapping
-  public void getAllClasses(){
+  public void getAllClasses() {
 
   }
 
+  //teacher
   @PostMapping
-  public void createClass(){
+  public void createClass() {
+
+  }
+
+  //teacher
+  @GetMapping("/{classId}/qr")
+  public void generateQR(@PathVariable Long classId) {
+
+  }
+
+
+  //teacher
+  @GetMapping("/{classId}/attendances")
+  public void generateClassAttendance(@PathVariable Long classId) {
+
+  }
+
+  //student
+  @PostMapping("/{classId}/students/{studentId}")
+  public void createAttendance(@PathVariable Long classId, @PathVariable Long studentId) {
 
   }
 
