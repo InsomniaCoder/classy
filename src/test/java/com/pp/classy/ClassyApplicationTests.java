058aec080e0b9e3d0e449e14aa533ef9b2af8f27
Tanat Lokejaroenlarb <tanatloke@gmail.com>
2020-05-30 01:14:07 +0700

    initial commit

---
+++ src/test/java/com/pp/classy/ClassyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.classy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
